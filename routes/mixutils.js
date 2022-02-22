abc4ca50447cbfda58c9040c9749630e287a48bc
g.sen <giu.sene@gmail.com>
2022-02-22 10:49:46 +0100

    add rent price to messages

--- routes/mixutils.js
+++ routes/mixutils.js
@@ -6,38 +6,49 @@ const Room = require("../models/room");
  * @param roomId - roomId created
  */
 exports.addRoomToUser = async (userId, roomId) => {
-  await User.updateOne({ _id: userId }, { $set: { roomId: roomId }})
-    .then((result) => {
+  await User.updateOne({ _id: userId }, { $set: { roomId: roomId } })
+    .then(result => {
       res.status(200).json(result);
     })
-    .catch((error) => {
+    .catch(error => {
       new Error(error);
     });
 };
 
-exports.removeRoomToUser = async (userId) => {
-  await User.updateOne({ _id: userId }, { $set: { roomId: resetRoomPreview() }})
-    .then((result) => {
-      console.log(result)
+exports.removeRoomToUser = async userId => {
+  await User.updateOne(
+    { _id: userId },
+    { $set: { roomId: resetRoomPreview() } }
+  )
+    .then(result => {
+      console.log(result);
       // res.status(200).json(result);
     })
-    .catch((error) => {
+    .catch(error => {
       new Error(error);
     });
 };
 
-exports.resetUser = async (userId) => {
-  await User.updateOne({ _id: userId }, { $set: { ilike: [] }, $set: { matches: [] }, $set: { wholikesme: [] }, $set: { newLike: [] }, $set: { newMatch: [] } })
-    .then((result) => {
+exports.resetUser = async userId => {
+  await User.updateOne(
+    { _id: userId },
+    {
+      $set: { ilike: [] },
+      $set: { matches: [] },
+      $set: { wholikesme: [] },
+      $set: { newLike: [] },
+      $set: { newMatch: [] },
+    }
+  )
+    .then(result => {
       res.status(200).json(result);
     })
-    .catch((error) => {
+    .catch(error => {
       new Error(error);
     });
 };
 
-
-exports.updateRoomPreview = (updates) => {
+exports.updateRoomPreview = updates => {
   return {
     roomId: updates._id.toString(),
     roomType: updates.roomType,
@@ -55,15 +66,15 @@ exports.updateRoomPreview = (updates) => {
 
 exports.resetRoomPreview = () => {
   return {
-    roomId: '',
-    roomType: '',
-    city: '',
-    town: '',
-    roomPhotos: '',
+    roomId: "",
+    roomType: "",
+    city: "",
+    town: "",
+    roomPhotos: "",
   };
 };
 
-exports.usersInterestedInRoom = (updates) => {
+exports.usersInterestedInRoom = updates => {
   return {
     iam: updates.iam,
     city: updates.city,
@@ -76,7 +87,7 @@ exports.usersInterestedInRoom = (updates) => {
   };
 };
 
-exports.previewWhoLikesMe = (updates) => {
+exports.previewWhoLikesMe = updates => {
   return {
     photo: updates.photo,
     name: updates.name,
@@ -86,15 +97,15 @@ exports.previewWhoLikesMe = (updates) => {
   };
 };
 
-const getUserInfo = async (usersId) => {
+const getUserInfo = async usersId => {
   let newUsers = [];
   let promises = [];
-  usersId.map(async (single) => {
+  usersId.map(async single => {
     promises.push(
       new Promise((resolve, reject) => {
         User.findOne()
           .where({ _id: single })
-          .then((result) => {
+          .then(result => {
             resolve(single);
             newUsers = [
               ...newUsers,
@@ -125,15 +136,15 @@ const getUserInfo = async (usersId) => {
   return newUsers;
 };
 
-const getRoomInfo = async (roomsId) => {
+const getRoomInfo = async roomsId => {
   let newRooms = [];
   let promises = [];
-  roomsId.map(async (single) => {
+  roomsId.map(async single => {
     promises.push(
       new Promise((resolve, reject) => {
         Room.findOne()
           .where({ _id: single })
-          .then((result) => {
+          .then(result => {
             resolve(single);
             newRooms = [
               ...newRooms,
@@ -160,47 +171,48 @@ const getRoomInfo = async (roomsId) => {
   return newRooms;
 };
 
-const getMessageInfo = async (messages) => {
+const getMessageInfo = async messages => {
   let newMessages = {};
   let promises = [];
-  Object.keys(messages).map(async (single) => {
-    single !== 'message' &&
-    promises.push(
-      new Promise((resolve, reject) => {
-        User.findOne()
-          .where({ _id: single })
-          .then((result) => {
-            resolve(single);
-            newMessages = {
-              ...newMessages,
-              [single]: {
-                discussion: messages[single],
-                user: {
-                  name: result.name,
-                  surname: result.surname,
-                  photo: result.photo,
-                  id: result.id,
-                  room: {
-                    roomId: result.roomId.roomId,
-                    roomType: result.roomId.roomType,
-                    city: result.roomId.city,
-                    town: result.roomId.town,
-                    roomPhotos: result.roomId.roomPhotos,
-                    friendlyWith: result.roomId.friendlyWith,
+  Object.keys(messages).map(async single => {
+    single !== "message" &&
+      promises.push(
+        new Promise((resolve, reject) => {
+          User.findOne()
+            .where({ _id: single })
+            .then(result => {
+              resolve(single);
+              newMessages = {
+                ...newMessages,
+                [single]: {
+                  discussion: messages[single],
+                  user: {
+                    name: result.name,
+                    surname: result.surname,
+                    photo: result.photo,
+                    id: result.id,
+                    room: {
+                      roomId: result.roomId.roomId,
+                      roomType: result.roomId.roomType,
+                      city: result.roomId.city,
+                      rentPrice: result.roomId.rentPrice,
+                      town: result.roomId.town,
+                      roomPhotos: result.roomId.roomPhotos,
+                      friendlyWith: result.roomId.friendlyWith,
+                    },
                   },
                 },
-              },
-            };
-          });
-      })
-    );
+              };
+            });
+        })
+      );
   });
 
   await Promise.all(promises);
   return newMessages;
 };
 
-exports.getUserData = async (updates) => {
+exports.getUserData = async updates => {
   return {
     _id: updates._id,
     name: updates.name,
@@ -218,70 +230,100 @@ exports.getUserData = async (updates) => {
       ilike: await getUserInfo(updates.roomId.ilike),
       matches: await getUserInfo(updates.roomId.matches),
     },
-    wholikesme: updates.roomId.roomId ? await getUserInfo(updates.wholikesme) : await getRoomInfo(updates.wholikesme),
-    ilike: updates.roomId.roomId ? await getUserInfo(updates.ilike) : await getRoomInfo(updates.ilike),
-    matches: updates.roomId.roomId ? await getUserInfo(updates.matches) : await getRoomInfo(updates.matches),
+    wholikesme: updates.roomId.roomId
+      ? await getUserInfo(updates.wholikesme)
+      : await getRoomInfo(updates.wholikesme),
+    ilike: updates.roomId.roomId
+      ? await getUserInfo(updates.ilike)
+      : await getRoomInfo(updates.ilike),
+    matches: updates.roomId.roomId
+      ? await getUserInfo(updates.matches)
+      : await getRoomInfo(updates.matches),
     messages: await getMessageInfo(updates.messages),
     token: updates.token,
     newLike: updates.newLike,
-    newMatch: updates.newMatch
+    newMatch: updates.newMatch,
   };
 };
 
 const updatedUserMatches = async function (userIlike, roomIlike) {
   // console.log(`MATCH! User likes the room `)
-  const updatedUserIlike = userIlike.ilike.filter(room => room !== roomIlike._id.toString());
-  const updatedUserWholikesme = userIlike.wholikesme.filter(room => room !== roomIlike._id.toString());
+  const updatedUserIlike = userIlike.ilike.filter(
+    room => room !== roomIlike._id.toString()
+  );
+  const updatedUserWholikesme = userIlike.wholikesme.filter(
+    room => room !== roomIlike._id.toString()
+  );
   const updatedUserMatches = [...userIlike.matches, roomIlike._id.toString()];
 
-  const updatedUserNewMatches = [...userIlike.newMatch, roomIlike._id.toString()];
-  const updatedUserNewLike = userIlike.newLike.filter(room => room !== roomIlike._id.toString());
+  const updatedUserNewMatches = [
+    ...userIlike.newMatch,
+    roomIlike._id.toString(),
+  ];
+  const updatedUserNewLike = userIlike.newLike.filter(
+    room => room !== roomIlike._id.toString()
+  );
 
-  const updatedRooomIlike = roomIlike.ilike.filter(user => user !== userIlike._id.toString());
-  const updatedRooomWholikesme = roomIlike.wholikesme.filter(user => user !== userIlike._id.toString());
+  const updatedRooomIlike = roomIlike.ilike.filter(
+    user => user !== userIlike._id.toString()
+  );
+  const updatedRooomWholikesme = roomIlike.wholikesme.filter(
+    user => user !== userIlike._id.toString()
+  );
   const updatedRooomMatches = [...roomIlike.matches, userIlike._id.toString()];
 
-
-  await User.findById({ _id: roomIlike.roomOwner })
-      .then(async (result) => {
-        let newNewMatch = [...result.newMatch, userIlike._id.toString()]
-        let newNewLike = result.newLike.filter((id) => id !== userIlike._id.toString());
-        await User.updateOne(
-          { _id: roomIlike.roomOwner }, { $set: { 
-            "roomId.ilike": updatedRooomIlike, 
-            "roomId.wholikesme": updatedRooomWholikesme, 
-            "roomId.matches": updatedRooomMatches,
-            "newLike": newNewLike,
-            "newMatch": newNewMatch 
-          } })
-      })
+  await User.findById({ _id: roomIlike.roomOwner }).then(async result => {
+    let newNewMatch = [...result.newMatch, userIlike._id.toString()];
+    let newNewLike = result.newLike.filter(
+      id => id !== userIlike._id.toString()
+    );
+    await User.updateOne(
+      { _id: roomIlike.roomOwner },
+      {
+        $set: {
+          "roomId.ilike": updatedRooomIlike,
+          "roomId.wholikesme": updatedRooomWholikesme,
+          "roomId.matches": updatedRooomMatches,
+          newLike: newNewLike,
+          newMatch: newNewMatch,
+        },
+      }
+    );
+  });
 
   await Room.updateOne(
-    { _id: roomIlike._id.toString() }, { $set: { 
-      "ilike": updatedRooomIlike, 
-      "wholikesme": updatedRooomWholikesme, 
-      "matches": updatedRooomMatches 
-    } });
+    { _id: roomIlike._id.toString() },
+    {
+      $set: {
+        ilike: updatedRooomIlike,
+        wholikesme: updatedRooomWholikesme,
+        matches: updatedRooomMatches,
+      },
+    }
+  );
 
   // await User.updateOne(
-  //   { _id: roomIlike.roomOwner }, { $set: { 
-  //     "roomId.ilike": updatedRooomIlike, 
-  //     "roomId.wholikesme": updatedRooomWholikesme, 
+  //   { _id: roomIlike.roomOwner }, { $set: {
+  //     "roomId.ilike": updatedRooomIlike,
+  //     "roomId.wholikesme": updatedRooomWholikesme,
   //     "roomId.matches": updatedRooomMatches,
   //     "newLike": '',
-  //     "newMatch": ''  
+  //     "newMatch": ''
   //   } })
 
   await User.updateOne(
-    { _id: userIlike._id.toString() }, { $set: { 
-      "ilike": updatedUserIlike, 
-      "wholikesme": updatedUserWholikesme, 
-      "matches": updatedUserMatches,
-      "newLike": updatedUserNewLike,
-      "newMatch": updatedUserNewMatches
-    } })
-
-}
+    { _id: userIlike._id.toString() },
+    {
+      $set: {
+        ilike: updatedUserIlike,
+        wholikesme: updatedUserWholikesme,
+        matches: updatedUserMatches,
+        newLike: updatedUserNewLike,
+        newMatch: updatedUserNewMatches,
+      },
+    }
+  );
+};
 //   // console.log(`MATCH! User likes the room `)
 //   const updatedUserIlike = userIlike.ilike.filter(
 //     (room) => room !== roomIlike._id.toString()
@@ -291,7 +333,6 @@ const updatedUserMatches = async function (userIlike, roomIlike) {
 //   );
 //   const updatedUserMatches = [...userIlike.matches, roomIlike._id.toString()];
 
-  
 //   //////
 //   const updatedUserNewMatches = [...userIlike.newMatch, roomIlike._id.toString()];
 
@@ -300,7 +341,7 @@ const updatedUserMatches = async function (userIlike, roomIlike) {
 //   );
 
 //   console.log(roomIlike)
-  
+
 //   const updatedRoomNewlike = roomIlike.newLike.filter(
 //     (user) => user !== userIlike._id.toString()
 //   );
@@ -310,10 +351,9 @@ const updatedUserMatches = async function (userIlike, roomIlike) {
 //   let updatedRoomNewMatches;
 
 //   await User.findById({ _id: roomIlike.roomOwner })
-//     .then((result) => 
+//     .then((result) =>
 //        updatedRoomNewMatches = [...result.newMatch, userIlike._id.toString()]
 //     )
-        
 
 //   const updatedRooomIlike = roomIlike.ilike.filter(
 //     (user) => user !== userIlike._id.toString()
@@ -366,22 +406,22 @@ const updatedUserMatches = async function (userIlike, roomIlike) {
  */
 exports.checkMatch = async (userId, roomId) => {
   const userIlike = await User.findById({ _id: userId })
-    .then((userData) => userData)
-    .catch((error) => {
+    .then(userData => userData)
+    .catch(error => {
       new Error(error);
     });
 
   const roomIlike = await Room.findById({ _id: roomId })
-    .then((roomData) => roomData)
-    .catch((error) => {
+    .then(roomData => roomData)
+    .catch(error => {
       new Error(error);
     });
 
   const roomMatched = userIlike.ilike?.length
-    ? userIlike.ilike.filter((room) => room === roomId)
+    ? userIlike.ilike.filter(room => room === roomId)
     : [];
   const userMatched = roomIlike.ilike?.length
-    ? roomIlike.ilike.filter((user) => user === userId)
+    ? roomIlike.ilike.filter(user => user === userId)
     : [];
 
   roomMatched.length > 0 && userMatched.length > 0
